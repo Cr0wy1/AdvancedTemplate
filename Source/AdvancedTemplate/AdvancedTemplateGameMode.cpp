21ff0a0640b93ea27a7248174756a90172bbbc14
Oskar Konczarek <o-konczarek@t-online.de>
2020-08-19 20:20:19 +0200

    Init Project

---
+++ Source/AdvancedTemplate/AdvancedTemplateGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "AdvancedTemplateGameMode.h"
+#include "AdvancedTemplateHUD.h"
+#include "AdvancedTemplateCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAdvancedTemplateGameMode::AAdvancedTemplateGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AAdvancedTemplateHUD::StaticClass();
+}
---
+++ Source/AdvancedTemplate/AdvancedTemplateGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedTemplateGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAdvancedTemplateGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdvancedTemplateGameMode();
+};
+
+
+
